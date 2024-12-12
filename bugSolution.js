9705a5cd23e9cd46d6cde6eb3e7741725e4a65a6
BugHunter-X <askzenai@gmail.com>
2024-12-12 05:16:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,42 @@
+```javascript
+import * as React from 'react';
+import { Camera, CameraType } from 'expo-camera';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  const [hasPermission, setHasPermission] = React.useState(null);
+  const [type, setType] = React.useState(CameraType.back);
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+if (hasPermission === null) {
+    return <View/>;
+  }
+if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ratio="16:9">
+        {/* Other Camera component code here */}
+      </Camera>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+});
+
+export default App;
+```
